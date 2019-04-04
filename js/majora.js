4f323e75ceb076ee49767f04a37668ef46df49b0
MiraiScarlet <37510263+MiraiScarlet@users.noreply.github.com>
2019-04-04 20:13:53 +0900

    Add files via upload

---
+++ js/majora.js
@@ -0,0 +1,238 @@
+var bingoList = [];
+bingoList[1] = [
+    { name: "ボムチュウ未取得", types: ["restrict"] },
+    { name: "宿の鍵", types: ["delivery"] },
+    { name: "バクレツのお面", types: ["allnight"] },
+    { name: "盾未所持", types: ["mirror"] },
+    { name: "4日目突入", types: ["end"] },
+];
+bingoList[2] = [
+    { name: "キータンクイズのハートのかけら", types: ["delivery"] },
+    { name: "Bボタンを空にする（剣未所持）", types: ["sword","epona"] },
+    { name: "Cボタンを2つ空にする", types: ["delivery"] },
+    { name: "タルミナ平原のマップの宝箱アイコンを3つ全て消す", types: ["map"] },
+    { name: "手帳のローザ姉妹にリボンをつける", types: ["notebook"] },
+    { name: "手帳の??にリボンをつける", types: ["notebook","deed"] }
+];
+bingoList[3] = [
+    { name: "ミルク", types: ["bottle","ranch"] },
+    { name: "青いクスリ", types: ["bottle"] },
+    { name: "魔法のマメ20個", types: ["slot","bean"] },
+    { name: "デクナッツのプレイスポットのハートのかけら", types: ["heart"] },
+    { name: "シャトー・ロマーニ", types: ["delivery","bottle"] },
+    { name: "ギブドのお面", types: ["mirror"] },
+    { name: "ポストハット", types: ["delivery"] }
+];
+bingoList[4] = [
+    { name: "ウッドフォールの神殿のマップとコンパス", types: ["wft","mc"] },
+    { name: "太陽のお面を救う", types: ["couple"] },
+    { name: "お面7つ", types: ["mask"] },
+    { name: "手帳にリボンを6個つける", types: ["notebook"] },
+    { name: "タルミナ平原のハートのかけら5つ全て", types: ["heart"] },
+    { name: "ウッドフォールの神殿のボス鍵", types: ["wft","bosskey"] }
+];
+bingoList[5] = [
+    { name: "オドルワのボス部屋の鍵を外す", types: ["wft","bosskey"] },
+    { name: "思い出のペンダント", types: ["couple","slot"] },
+    { name: "ウサギずきん", types: ["ranch"] },
+    { name: "エポナの歌", types: ["song","epona","ranch"] },
+    { name: "ドッグレースのハートのかけら", types: ["ranch"] },
+    { name: "手帳のポストマンにリボンをつける", types: ["notebook","delivery","couple"] },
+    { name: "ウッドフォールの神殿のはぐれ妖精10匹", types: ["fairy","wft"] }
+];
+bingoList[6] = [
+    { name: "フックショット", types: ["hook","fortress","stt","gbt"] },
+    { name: "海のクモ館の黄金のスタルチュラの魂15個", types: ["spider","wallet"] },
+    { name: "海のクモ館のハートのかけら", types: ["wallet"] },
+    { name: "嵐の歌", types: ["song","grave"] },
+    { name: "アイアンナックを2体倒す", types: ["song","grave"] },
+    { name: "フェザーソード", types: ["sword"] },
+    { name: "隊長のボウシ", types: ["grave"] },
+    { name: "スタルキータを倒す", types: ["grave"] }
+];
+bingoList[7] = [
+    { name: "エポナのやぶさめ35秒未満", types: ["song","epona","ranch"] },
+    { name: "タツノオトシゴ", types: ["picto","fortress","bottle"] },
+    { name: "イカーナ古城のハートのかけら", types: ["castle"] },
+    { name: "海洋研究所の魚のハートのかけら", types: ["heart"] },
+    { name: "大翼の歌未取得", types: ["song","restrict","stt","istt"] },
+    { name: "隠しフクロウ像を叩くの禁止", types: ["restrict","stt","istt","0thday"] },
+    { name: "オドルワの亡骸", types: ["wft","remains"] },
+    { name: "墓を2つあばく", types: ["grave"] }
+];
+bingoList[8] = [
+    { name: "ゾーラのみさきのマップの宝箱アイコンを3つ全て消す", types: ["hook","fortress","map"] },
+    { name: "最大の矢立て", types: ["wft"] },
+    { name: "誓いの号令", types: ["song","remains"] },
+    { name: "すべてのエリアマップ", types: ["map"] },
+    { name: "山里からスノーヘッドへの道のハートのかけら", types: ["heart"] },
+    { name: "ウッドフォールの神殿の宝箱を8つ開ける", types: ["wft"] },
+    { name: "ゴロンの仮面", types: ["lens"] },
+    { name: "石コロのお面", types: ["lens"] }
+];
+bingoList[9] = [
+    { name: "特上ロース岩を持ち上げている状態", types: ["lens","end"] },
+    { name: "ゲーロのお面", types: ["lens"] },
+    { name: "光の矢", types: ["elemental","stt","gfs"] },
+    { name: "ロックビルの神殿のマップとコンパス", types: ["stt","mc"] },
+    { name: "ロックビルの神殿の未使用の小さな鍵2つ", types: ["stt","gfs"] }
+];
+bingoList[10] = [
+    { name: "踊っているリーデットの写真", types: ["picto","mirror"] },
+    { name: "井戸の下の鉄格子を5つ解除する", types: ["mirror"] },
+    { name: "幽霊小屋のハートのかけら", types: ["heart"] },
+    { name: "ロックビルの神殿のはぐれ妖精5匹", types: ["stt","gistt","gfs"] },
+    { name: "魔法のマメの入った空箱を開ける", types: ["bean","stt","istt","gistt"] },
+    { name: "ロックビルの神殿の黄色の床スイッチを3つ押す", types: ["stt","istt","gistt"] }
+];
+bingoList[11] = [
+    { name: "アキンドナッツのハートのかけら4つ", types: ["heart","scrub","deed"] },
+    { name: "トンガリ岩のハートのかけら", types: ["picto","fortress"] },
+    { name: "巨人のサイフ", types: ["wallet","spider"] },
+    { name: "サイフに500ルピー", types: ["wallet","spider"] },
+    { name: "サイフにぴったり333ルピー", types: ["wallet","spider"] },
+    { name: "デク姫（空き瓶に入れた）", types: ["wft","remains"] },
+    { name: "ポウ", types: ["stt","istt","gistt"] },
+    { name: "巨人の仮面", types: ["stt","istt","gistt"] }
+];
+bingoList[12] = [
+    { name: "まことのお面", types: ["spider"] },
+    { name: "両方の射的場のハートのかけら2つ", types: ["heart","wft"] },
+    { name: "めおとの面", types: ["couple"] },
+    { name: "座長のお面", types: ["mask","notebook"] },
+    { name: "イカーナのアキンドナッツから200ルピーを貰う", types: ["scrub","deed"] },
+    { name: "最大のボム袋", types: ["scrub","lens"] },
+    { name: "ハート6つ（増殖は含まない）", types: ["heart"] },
+    { name: "銀行に1000ルピー預ける", types: ["wallet"] },
+    { name: "海賊のアジトのハートのかけら", types: ["fortress"] },
+    { name: "ロックビルの神殿のウィズローブのはぐれ妖精", types: ["stt","istt","gistt"] },
+    { name: "ロックビルの神殿のボス鍵", types: ["stt","istt","bosskey","gistt"] }
+];
+bingoList[13] = [
+    { name: "手帳にリボンを10個つける", types: ["notebook"] },
+    { name: "大回転斬り", types: ["fairy","wft"] },
+    { name: "半分だけの子守唄", types: ["lens","song"] },
+    { name: "コレクト画面の上段の歌全部", types: ["song","grave","epona"] },
+    { name: "ガロのお面", types: ["ranch","epona"] },
+    { name: "ツインモルドのボス部屋の鍵を外す", types: ["dungeon","stt","istt","bosskey","gistt"] },
+    { name: "ツインモルドの亡骸", types: ["dungeon","stt","istt","remains","gistt"] },
+    { name: "ダイナフォスを3体倒す", types: ["elemental","gbt"] }
+];
+bingoList[14] = [
+    { name: "井戸の下の宝箱を2個開ける", types: ["mirror"] },
+    { name: "海賊の砦の宝箱を11個開ける", types: ["fortress","hook"] },
+    { name: "ゴロンのララバイ", types: ["lens","song"] },
+    { name: "大バクダン", types: ["lens"] },
+    { name: "ガロを倒す", types: ["epona"] },
+    { name: "ロマーニのお面", types: ["ranch"] },
+    { name: "ロックビルの神殿のはぐれ妖精10匹", types: ["stt","istt","gistt","gfs"] }
+];
+bingoList[15] = [
+    { name: "夜更かしのお面", types: ["allnight","spider","wallet"] },
+    { name: "お面10個", types: ["mask"] },
+    { name: "手帳のロマニーにリボンをつける", types: ["notebook","epona"] },
+    { name: "ビッグポウ", types: ["grave"] },
+    { name: "空き瓶3つ（増殖は含めない）", types: ["bottle"] },
+    { name: "お面画面の縦列のお面を4つ揃える", types: ["mask"] },
+    { name: "魔法のマメを8つ育てる", types: ["bean"] },
+    { name: "黄金のスタルチュラの魂45個", types: ["spider","wallet"] },
+    { name: "イカーナ古城の天井に穴を開ける", types: ["lens","castle"] },
+    { name: "ウィズローブを2体倒す", types: ["elemental","gbt"] }
+];
+bingoList[16] = [
+    { name: "隊長のボウシとギブドのお面とガロのお面", types: ["mask","grave","epona"] },
+    { name: "お面画面の横列のお面を6つ揃える", types: ["mask"] },
+    { name: "薬３種類全て(赤・緑・青）", types: ["bottle"] },
+    { name: "お面12個", types: ["mask"] },
+    { name: "コレクト画面下段の歌を3つ", types: ["song"] }
+];
+bingoList[17] = [
+    { name: "沼地のハートのかけら3つ全部", types: ["heart","picto","remains","wft"] },
+    { name: "Cアイテム画面のアイテムを斜めに4つ揃える", types: ["slot","bottle"] },
+    { name: "ブーさんのお面", types: ["wft","remains"] },
+    { name: "魔法のキノコ", types: ["wft","remains"] },
+    { name: "スノーヘッドの神殿のマップとコンパス", types: ["sht","mc","gsht"] },
+    { name: "炎の矢", types: ["sht","elemental","gsht"] },
+    { name: "スノーヘッドの神殿のボス鍵", types: ["sht","bosskey","gsht"] },
+    { name: "クロックタウン(東)で手に入るハートのかけら5つ", types: ["heart"] },
+    { name: "潮騒のボサノバ", types: ["song","fortress","gbt","gsht","gistt"] },
+    { name: "スノーヘッドの神殿のはぐれ妖精10匹", types: ["sht","gsht"] }
+];
+bingoList[18] = [
+    { name: "ハート7つ（増殖は含まない）", types: ["heart"] },
+    { name: "ミラーシールド", types: ["mirror"] },
+    { name: "Cアイテム画面を12個埋める", types: ["slot","bottle"] },
+    { name: "スノーヘッドの神殿の未使用の小さな鍵3つ", types: ["sht","gsht"] },
+    { name: "ゴートのボス部屋の鍵を外す", types: ["sht","bosskey","gsht"] },
+    { name: "未使用の小さな鍵を合計で4つ", types: ["sht","gsht"] },
+    { name: "属性矢2種", types: ["elemental"] },
+    { name: "神殿のコンパス3つ", types: ["mc"] },
+    { name: "神殿のマップとコンパス2つずつ", types: ["mc"] },
+    { name: "ビーバーレースの空き瓶", types: ["hook","bottle"] }
+];
+bingoList[19] = [
+    { name: "空き瓶4つ（増殖は含まない）", types: ["bottle"] },
+    { name: "ボス鍵2つ", types: ["bosskey"] },
+    { name: "手帳のゴーマン兄弟にリボンをつける", types: ["notebook","epona","ranch"] },
+    { name: "ゴートの亡骸", types: ["sht","remains","gsht"] }
+];
+bingoList[20] = [
+    { name: "手帳に写真を全て", types: ["notebook"] },
+    { name: "ビーバーレースのハートのかけら", types: ["hook","bottle"] },
+    { name: "手帳のマニ屋のおじさんにリボンをつける", types: ["notebook","allnight","spider"] },
+    { name: "フクロウ像を10個全て叩く", types: ["hook","stt"] },
+    { name: "はぐれ妖精を合計で20匹", types: ["fairy"] }
+];
+bingoList[21] = [
+    { name: "歌7つ", types: ["song"] },
+    { name: "空き瓶の中身10種", types: ["bottle"] },
+    { name: "お面14個", types: ["mask"] },
+    { name: "スノーヘッドの神殿の柱のブロックを全て取り除く", types: ["lens","sht","gsht"] },
+    { name: "山里のマップの宝箱アイコンを3つ消す", types: ["sht","remains","map","gsht"] },
+    { name: "グレートベイの神殿のマップとコンパス", types: ["dungeon","gbt","mc","gsht","gistt"] }
+];
+bingoList[22] = [
+    { name: "スノーヘッドの神殿の宝箱を10個開ける", types: ["sht","gsht"] },
+    { name: "亡骸2つ", types: ["remains"] },
+    { name: "グレートベイの神殿の宝箱を5つ開ける", types: ["dungeon","gbt","gsht","gistt"] },
+    { name: "スノーヘッドの神殿の氷を15個溶かす", types: ["sht","elemental","gsht"] },
+    { name: "神殿のマップとコンパス3つずつ", types: ["mc"] }
+];
+bingoList[23] = [
+    { name: "グヨーグの亡骸", types: ["dungeon","gbt","remains","gsht","gistt"] },
+    { name: "ぬけがらのエレジー", types: ["song","mirror","castle"] },
+    { name: "魔力2倍", types: ["sht","lens"] },
+    { name: "砂金", types: ["dungeon","sht","remains","bottle","gsht"] },
+    { name: "神殿のボス鍵3つ", types: ["bosskey"] }
+];
+bingoList[24] = [
+    { name: "グレートベイの神殿の水流を変える", types: ["dungeon","gbt","gsht","gistt"] },
+    { name: "ハート8つ（増殖は含まない）", types: ["heart"] },
+    { name: "クリミアの報酬（ハグか200ルピー）", types: ["ranch","0thday"] },
+    { name: "神殿のマップとコンパスとボス鍵2つ", types: ["mc","bosskey"] }
+];
+bingoList[25] = [
+    { name: "氷の矢", types: ["dungeon","elemental","gbt","gsht","gistt"] },
+    { name: "金剛の剣", types: ["dungeon","sword","sht","remains"] },
+    { name: "はぐれ妖精を合計で30匹", types: ["dungeon","fairy"] },
+    { name: "大妖精の報酬2つ", types: ["gfairy"] },
+    { name: "大妖精の剣", types: ["gfs","gfairy","gbt"] }
+];
+$(function() { srl.bingo(bingoList, 5); });
+
+// この和訳ではGitHubにて公開されている物を使用しています
+// 新規タスクはそれに似せて翻訳しています
+var bname = "LoZ MM";
+var bversion = "12-30-16";
+
+// 没タスク
+// グヨーグのボス部屋の鍵を外す
+// グレートベイの神殿のゲッコーを倒す
+// グレートベイの神殿のはぐれ妖精10匹
+// グレートベイの神殿の氷を3つ溶かす
+// グレートベイの神殿の緑色のポンプを稼働させる
+// パメラが家の外に出た写真
+// 各神殿のはぐれ妖精を7匹ずつ
+// 手帳にリボンを15個つける
+// 全ての神殿のマップ
+// 属性の矢1種
